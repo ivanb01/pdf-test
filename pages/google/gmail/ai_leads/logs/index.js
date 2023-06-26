7fba01e1e5ee6be8cdea148f86fadd9fea1c680c
Edon Mustafa <edon@opgny.com>
2023-06-26 18:56:31 +0200

    better ux/ui for logs

--- pages/google/gmail/ai_leads/logs/index.js
+++ pages/google/gmail/ai_leads/logs/index.js
@@ -37,65 +37,61 @@ export default function GmailAILogs() {
   return (
     <div className="container mx-auto p-6">
       <h1 className="text-2xl font-bold mb-4">OxfordAI Labs</h1>
-      <p className="mb-4">Early preview: Gmail AI Logs</p>
-
-          
       <div>
-        <h2 className="text-xl font-bold mb-2">Gmail AI Logs:</h2>
+        <h2 className="text-xl mb-2">Gmail AI Logs <span className='text-sm'>(early preview)</span></h2>
         {logs.length > 0 ? (
           <table className="table-auto w-full">
             <thead>
               <tr>
-                
-                <th className="px-4 py-2">Email from:</th>
-                <th className="px-4 py-2">Category</th>
-                <th className="px-4 py-2">Summary</th>
+                <th className="px-4 py-2 text-left">From</th>
+                <th className="px-4 py-2 text-left">💬 Category</th>
+                <th className="px-4 py-2 text-left">💬 Summary</th>
               </tr>
             </thead>
             <tbody>
-              {logs.map((log, index) => (
-                <React.Fragment key={index}>
-                  <tr
-                    className={logs.result_contact_stored_in_db ? 'bg-green2' : 'bg-gray2'}
-                    onClick={() => toggleRow(index)}
-                  >
-                    
-                    <td className="px-4 py-2">{log.email_from}</td>
-                    <td className="px-4 py-2">
-                      {(log.ai_contact_category_1 || log.ai_contact_category_2) ? (
-                        `${log.ai_contact_category_1 || ''} ${log.ai_contact_category_2 || ''}`
-                      ) : (
-                        log.result_contact_category_id === 1 ? 'Uncategorized' : log.result_contact_category_id
-                      )}
+            {logs.map((log, index) => (
+              <React.Fragment key={index}>
+                <tr
+                  className={`${
+                    logs.result_contact_stored_in_db ? 'bg-green2' : 'bg-gray1'
+                  } cursor-pointer hover:bg-gray-200 transition-colors duration-200 ease-in-out`}
+                  onClick={() => toggleRow(index)}
+                >
+                  <td className="px-4 py-2">
+                    {expandedRows.includes(index) ? '−' : '+'} {log.email_from}
+                  </td>
+                  <td className="px-4 py-2">
+                    {(log.ai_contact_category_1 || log.ai_contact_category_2) ? (
+                      `${log.ai_contact_category_1 || ''} ${log.ai_contact_category_2 || ''}`
+                    ) : (
+                      log.result_contact_category_id === 1 ? 'Uncategorized' : log.result_contact_category_id
+                    )}
+                  </td>
+                  <td className="px-4 py-2">
+                    {log.ai_email_summary ? (
+                      `${log.ai_email_summary} - `
+                    ) : (
+                      'No AI summary - '
+                    )}
+                    <a href={log.email_link} className="text-blue-500" target="_blank" rel="noopener noreferrer">
+                      (view mail) <i class="fas fa-arrow-right"></i>
+                    </a>
+                  </td>
+                </tr>
+                {expandedRows.includes(index) && (
+                  <tr>
+                    <td colSpan="4" className="px-4 py-2">
+                      <pre>{JSON.stringify(log.result_data, null, 2)}</pre>
                     </td>
-
-                    <td className="px-4 py-2">
-                      {log.ai_email_summary ? (
-                        `${log.ai_email_summary} - `
-                      ) : (
-                        'No AI summary - '
-                      )}
-                      <a href={log.email_link} className="text-blue-500" target="_blank" rel="noopener noreferrer">
-                        (view mail) <i class="fas fa-arrow-right"></i>
-                      </a>
-                    </td>
-
-
-
                   </tr>
-                  {expandedRows.includes(index) && (
-                    <tr>
-                      <td colSpan="4" className="px-4 py-2">
-                        <pre>{JSON.stringify(log.result_data, null, 2)}</pre>
-                      </td>
-                    </tr>
-                  )}
-                </React.Fragment>
-              ))}
+                )}
+              </React.Fragment>
+            ))}
+
             </tbody>
           </table>
         ) : (
-          <p>...</p>
+          <p>💬 thinking ...</p>
         )}
       </div>
     </div>
