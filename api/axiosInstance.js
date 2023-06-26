86b0169fb2d3740af2890f4688f792442b8e9bb5
Blendar Kabashi <blendar@opgny.com>
2023-06-26 10:39:23 +0200

    bugfix

--- api/axiosInstance.js
+++ api/axiosInstance.js
@@ -5,30 +5,36 @@ const axiosInstance = axios.create();
 
 axiosInstance.interceptors.request.use(
   async (config) => {
-    const apiGatewayUrl = localStorage.getItem('apiGatewayUrl');
+    setTimeout(() => {
+      const apiGatewayUrl = localStorage.getItem('apiGatewayUrl');
 
-    const appClientId = localStorage.getItem('appClientId');
-    const email = localStorage.getItem(
-      'CognitoIdentityServiceProvider.' + appClientId + '.LastAuthUser'
-    );
-    let token = localStorage.getItem(
-      'CognitoIdentityServiceProvider.' + appClientId + '.' + email + '.idToken'
-    );
-    console.log('data:', appClientId, email);
-    console.log('token', token);
-    // let currentSession = localStorage.getItem('currentSession')
-    //   ? JSON.parse(localStorage.getItem('currentSession'))
-    //   : await Auth.currentSession();
-    // let token = currentSession.idToken.jwtToken;
+      const appClientId = localStorage.getItem('appClientId');
+      const email = localStorage.getItem(
+        'CognitoIdentityServiceProvider.' + appClientId + '.LastAuthUser'
+      );
+      let token = localStorage.getItem(
+        'CognitoIdentityServiceProvider.' +
+          appClientId +
+          '.' +
+          email +
+          '.idToken'
+      );
+      console.log('data:', appClientId, email);
+      console.log('token', token);
+      // let currentSession = localStorage.getItem('currentSession')
+      //   ? JSON.parse(localStorage.getItem('currentSession'))
+      //   : await Auth.currentSession();
+      // let token = currentSession.idToken.jwtToken;
 
-    if (token) {
-      console.log('done auth currentsession');
-    }
+      if (token) {
+        console.log('done auth currentsession');
+      }
 
-    config.baseURL = apiGatewayUrl;
-    config.headers.common['Accept'] = 'application/json';
-    config.headers.common['Authorization'] = `Bearer ${token}`;
-    return config;
+      config.baseURL = apiGatewayUrl;
+      config.headers.common['Accept'] = 'application/json';
+      config.headers.common['Authorization'] = `Bearer ${token}`;
+      return config;
+    }, 500);
   },
   (error) => {
     return Promise.reject(error);
