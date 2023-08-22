d662532c36a44c388833bbfaa5d0b7dd5a1e29ff
Blendar Kabashi <blendar@opgny.com>
2023-08-22 11:50:59 +0200

    change edit popup

--- src/components/client-details-sidebar/card/index.js
+++ src/components/client-details-sidebar/card/index.js
@@ -9,6 +9,7 @@ import FilterDropdown from 'components/shared/dropdown/FilterDropdown';
 import DeleteClientOverlay from 'components/overlays/delete-client';
 import EditClientOverlay from 'components/overlays/edit-client';
 import { getInitials, phoneNumberFormat } from 'global/functions';
+import ReviewContact from '@components/overlays/review-contact';
 
 export default function ClientCard({ client }) {
   const [editingContact, setEditingContact] = useState(false);
@@ -85,12 +86,7 @@ export default function ClientCard({ client }) {
 
       {deletingContact && <DeleteClientOverlay handleCloseOverlay={() => setDeletingContact(false)} contact={client} />}
       {editingContact && (
-        <EditClientOverlay
-          handleClose={() => setEditingContact(false)}
-          title="Edit Contact"
-          className="w-[635px]"
-          client={client}
-        />
+        <ReviewContact handleClose={() => setEditingContact(false)} client={client} title="Edit Contact" />
       )}
     </>
   );
