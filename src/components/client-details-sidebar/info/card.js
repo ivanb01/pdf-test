32ea58955fb430e5b1e1675a16bce32cf96bc3f4
Blendar Kabashi <blendar@opgny.com>
2023-08-22 13:40:49 +0200

    fix bug

--- src/components/client-details-sidebar/info/card.js
+++ src/components/client-details-sidebar/info/card.js
@@ -3,6 +3,7 @@ import Chip from 'components/shared/chip';
 import Text from 'components/shared/text';
 
 export default function InfoCard({ label, showDot, content, iconContent, client }) {
+  console.log(content);
   return (
     <div className="flex flex-col my-3">
       <Text className="text-gray6 mb-1" h4>
@@ -10,7 +11,7 @@ export default function InfoCard({ label, showDot, content, iconContent, client 
       </Text>
       <div className="flex flex-row">
         {typeof content === 'object' ? (
-          content.map((chip) => <Chip notClickable secondary key={chip} label={chip} />)
+          content?.map((chip) => <Chip notClickable secondary key={chip} label={chip} />)
         ) : iconContent ? (
           <div className="flex flex-row items-center">
             <Text className="text-gray7" p>
