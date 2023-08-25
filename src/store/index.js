c48677d996fbe9f740468743687f8e7527641771
Blendar Kabashi <blendar@opgny.com>
2023-08-25 10:32:44 +0200

    refactor and update to latest dependencies

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import contactReducer from './contact/slice';
-import campaignsReducer from './campaign/slice';
-import clientsReducer from './client/slice';
-import eventsReducer from './event/slice';
 import contactsReducer from './contacts/slice';
 import globalReducer from './global/slice';
 import clientDetailsReducer from './clientDetails/slice';
 
 export default configureStore({
   reducer: {
-    contact: contactReducer,
-    campaigns: campaignsReducer,
-    clients: clientsReducer,
     clientDetails: clientDetailsReducer,
-    events: eventsReducer,
     contacts: contactsReducer,
     global: globalReducer,
   },
