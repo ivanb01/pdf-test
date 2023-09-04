d2e6f4cf5474afc86dd4bd9d47d0ec0bb1f9b412
Blendar Kabashi <blendar@opgny.com>
2023-09-04 15:44:55 +0200

    bugfix for dropdown

--- src/components/shared/dropdown/index.js
+++ src/components/shared/dropdown/index.js
@@ -44,34 +44,30 @@ const Dropdown = ({
         onChange={(val) => {
           props?.noOptionChange ? null : setSelected(val);
           handleSelect(val);
-        }}
-      >
+        }}>
         {({ open }) => (
           <>
-            <div className={horizontal && 'flex items-center'}>
+            <div className={`${horizontal && 'flex items-center'}`}>
               {label && (
                 <Listbox.Label className={`block text-sm font-medium text-gray6 mr-2`}>
                   {label} {optional && <span className="text-gray3 ml-1">(Optional)</span>}
                 </Listbox.Label>
               )}
-              <div className="mt-1 relative">
+              <div className={`mt-1 relative ${open && 'pb-64'}`}>
                 <Listbox.Button
                   className={`${selectClasses ? selectClasses : 'bg-white border rounded-md'} ${inputWidth} relative ${
                     !inputWidth && 'w-full'
-                  } border-gray-300 pl-3 pr-10 py-[9px] text-left cursor-pointer focus:outline-none focus:ring-1 focus:ring-blue1 focus:border-blue1 sm:text-sm`}
-                >
+                  } border-gray-300 pl-3 pr-10 py-[9px] text-left cursor-pointer focus:outline-none focus:ring-1 focus:ring-blue1 focus:border-blue1 sm:text-sm`}>
                   <span
                     className={`${
                       selectedOption === 'statusColor' && `absolute top-4 ${selected?.color} w-2 h-2 mr-2 rounded-full`
-                    }`}
-                  ></span>
+                    }`}></span>
 
                   {/* <span className={`flex items-center truncate capitalize ${selectedOption === 'statusColor' &&  `before:${selected.color} before:content-[''] before:w-2 before:h-2 before:mr-2 before:rounded-full`}` }> */}
                   <span
                     className={`flex items-center truncate capitalize ${!selected && placeHolder && 'text-gray6'} ${
                       selected && selected.name && 'text-gray8'
-                    } ${selectedOption === 'statusColor' && selected && 'pl-4'}`}
-                  >
+                    } ${selectedOption === 'statusColor' && selected && 'pl-4'}`}>
                     {!selected && placeHolder && placeHolder}
                     {selected && selected.name}
                   </span>
@@ -85,8 +81,7 @@ const Dropdown = ({
                   as={Fragment}
                   leave="transition ease-in duration-100"
                   leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-                >
+                  leaveTo="opacity-0">
                   <Listbox.Options className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
                     {options?.map((option) => (
                       <Listbox.Option
@@ -97,16 +92,14 @@ const Dropdown = ({
                             'cursor-pointer select-none relative py-2 pl-3 pr-9',
                           )
                         }
-                        value={option}
-                      >
+                        value={option}>
                         {({ selected, active }) => (
                           <>
                             <span
                               className={classNames(
                                 selected ? 'font-semibold' : 'font-normal',
                                 'block truncate capitalize',
-                              )}
-                            >
+                              )}>
                               {option.name}
                             </span>
 
@@ -115,8 +108,7 @@ const Dropdown = ({
                                 className={classNames(
                                   active ? 'text-white' : 'text-blue2',
                                   'absolute inset-y-0 right-0 flex items-center pr-4 capitalize',
-                                )}
-                              >
+                                )}>
                                 <CheckIcon className="h-5 w-5" aria-hidden="true" />
                               </span>
                             ) : null}
