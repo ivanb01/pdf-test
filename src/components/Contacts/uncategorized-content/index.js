366d993ac20c60b522f2f11b80f83ed5a276be65
erzaopgny <142299390+erzaopgny@users.noreply.github.com>
2023-09-14 16:16:25 +0200

    test

--- src/components/Contacts/uncategorized-content/index.js
+++ src/components/Contacts/uncategorized-content/index.js
@@ -120,6 +120,9 @@ const Uncategorized = ({
     );
   };
 
+  useEffect(() => {
+    console.log('test');
+  }, []);
   return (
     <>
       <div className="absolute left-0 top-0 right-0 bottom-0 flex flex-col">
