471164055cfd90219dcbd96b7d0799b94c22fc8b
Blendar Kabashi <blendar@opgny.com>
2023-10-03 16:04:59 +0200

    potential fix

--- src/components/shared/table/index.js
+++ src/components/shared/table/index.js
@@ -1210,143 +1210,152 @@ const Table = ({
           </tr>
         </thead>
         <tbody className="bg-white">
-          {professionalTypes.map((type, index) =>
-            contacts.filter((contact) => contact.category_id == type.id).length ? (
-              <>
-                <tr key={type.id} className={`contact-row border-b border-gray-200`}>
-                  <td colSpan="10">
-                    <div className="flex items-center px-6 py-2">
-                      <Text chipText className="text-gray4">
-                        {type.name == 'Vendor' ? 'Other Vendors' : type.name}
-                      </Text>
-                    </div>
-                  </td>
-                </tr>
-                {contacts
-                  .filter(
-                    (contact) =>
-                      searchTerm.split(' ').every((word) => {
-                        const lowercaseWord = word.toLowerCase();
-                        return (
-                          contact.first_name.toLowerCase().includes(lowercaseWord) ||
-                          contact.last_name.toLowerCase().includes(lowercaseWord)
-                        );
-                      }) && contact.category_id == type.id,
-                  )
-                  .map((contact) => (
-                    <tr
-                      key={contact.id}
-                      className="hover:bg-lightBlue1 cursor-pointer contact-row border-b border-gray-200"
-                      onClick={() =>
-                        router.push({
-                          pathname: '/contacts/details',
-                          query: { id: contact.id },
-                        })
-                      }>
-                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
-                        <ContactInfo
-                          data={{
-                            name: contact.first_name + ' ' + contact.last_name,
-                            email: contact.email,
-                            image: contact.profile_image_path,
-                          }}
-                        />
-                      </td>
-                      {/* <td className="whitespace-nowrap px-3 py-4 text-center text-sm text-gray-500">
+          {professionalTypes &&
+            professionalTypes.map((type, index) =>
+              contacts.filter((contact) => contact.category_id == type.id).length ? (
+                <>
+                  <tr key={type.id} className={`contact-row border-b border-gray-200`}>
+                    <td colSpan="10">
+                      <div className="flex items-center px-6 py-2">
+                        <Text chipText className="text-gray4">
+                          {type.name == 'Vendor' ? 'Other Vendors' : type.name}
+                        </Text>
+                      </div>
+                    </td>
+                  </tr>
+                  {contacts
+                    .filter(
+                      (contact) =>
+                        searchTerm.split(' ').every((word) => {
+                          const lowercaseWord = word.toLowerCase();
+                          return (
+                            contact.first_name.toLowerCase().includes(lowercaseWord) ||
+                            contact.last_name.toLowerCase().includes(lowercaseWord)
+                          );
+                        }) && contact.category_id == type.id,
+                    )
+                    .map((contact) => (
+                      <tr
+                        key={contact.id}
+                        className="hover:bg-lightBlue1 cursor-pointer contact-row border-b border-gray-200"
+                        onClick={() =>
+                          router.push({
+                            pathname: '/contacts/details',
+                            query: { id: contact.id },
+                          })
+                        }>
+                        <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+                          <ContactInfo
+                            data={{
+                              name: contact.first_name + ' ' + contact.last_name,
+                              email: contact.email,
+                              image: contact.profile_image_path,
+                            }}
+                          />
+                        </td>
+                        {/* <td className="whitespace-nowrap px-3 py-4 text-center text-sm text-gray-500">
                         <div className="text-gray7 font-medium bg-gray1 text-[10px] uppercase rounded min-w-[50px] h-6 flex items-center justify-center">
                           {contact.category_2}
                         </div>
                       </td> */}
-                      <td className="whitespace-nowrap px-3 py-4 text-center text-sm text-gray-500">
-                        <div className={'flex gap-1.5 items-center justify-center'}>
-                          {getSource(contact.import_source_text, contact.approved_ai).icon}
-                          <p className={'text-xs leading-4 font-medium text-gray8'}>
-                            {getSource(contact.import_source_text, contact.approved_ai).name}
-                          </p>
-                        </div>
-                        {contact.summary !== null && (
-                          <TooltipComponent
-                            side={'bottom'}
-                            align={'center'}
-                            triggerElement={
-                              <div
-                                className={
-                                  'mx-auto max-w-[239px] leading-5 text-left font-medium text-[11px] px-3 py-0.5 mt-1.5 text-ellipsis overflow-hidden bg-lightBlue1 text-lightBlue3 '
-                                }>
-                                {contact.summary}
+                        <td className="whitespace-nowrap px-3 py-4 text-center text-sm text-gray-500">
+                          <div className={'flex gap-1.5 items-center justify-center'}>
+                            {getSource(contact.import_source_text, contact.approved_ai).icon}
+                            <p className={'text-xs leading-4 font-medium text-gray8'}>
+                              {getSource(contact.import_source_text, contact.approved_ai).name}
+                            </p>
+                          </div>
+                          {contact.summary !== null && (
+                            <TooltipComponent
+                              side={'bottom'}
+                              align={'center'}
+                              triggerElement={
+                                <div
+                                  className={
+                                    'mx-auto max-w-[239px] leading-5 text-left font-medium text-[11px] px-3 py-0.5 mt-1.5 text-ellipsis overflow-hidden bg-lightBlue1 text-lightBlue3 '
+                                  }>
+                                  {contact.summary}
+                                </div>
+                              }>
+                              <div className={`w-[260px] pointer-events-none text-white bg-neutral1 rounded-lg`}>
+                                <p className="text-xs leading-4 font-normal">{contact.summary}</p>
                               </div>
-                            }>
-                            <div className={`w-[260px] pointer-events-none text-white bg-neutral1 rounded-lg`}>
-                              <p className="text-xs leading-4 font-normal">{contact.summary}</p>
-                            </div>
-                          </TooltipComponent>
-                        )}
-                      </td>
-                      <td>
-                        <div className="px-4 py-[10px] flex items-center justify-center">
-                          <div
-                            className="cursor-pointer relative rounded-full p-1.5 bg-gray1 hover:bg-gray2 mr-2 flex items-center justify-center"
-                            onMouseEnter={() => {
-                              document
-                                .querySelector('#tooltip-edit-contact-' + contact.id)
-                                .classList.remove('invisible', 'opacity-0');
-                              document.querySelector('#edit-contact-icon-' + contact.id).classList.add('text-gray4');
-                              document.querySelector('#edit-contact-icon-' + contact.id).classList.remove('text-gray3');
-                            }}
-                            onMouseLeave={() => {
-                              document
-                                .querySelector('#tooltip-edit-contact-' + contact.id)
-                                .classList.add('invisible', 'opacity-0');
-                              document.querySelector('#edit-contact-icon-' + contact.id).classList.add('text-gray3');
-                              document.querySelector('#edit-contact-icon-' + contact.id).classList.remove('text-gray4');
-                            }}
-                            onClick={(e) => {
-                              e.stopPropagation();
-                              handleCardEdit(contact);
-                            }}>
-                            <Edit id={'edit-contact-icon-' + contact.id} className="text-gray3 w-4 h-4" />
+                            </TooltipComponent>
+                          )}
+                        </td>
+                        <td>
+                          <div className="px-4 py-[10px] flex items-center justify-center">
                             <div
-                              id={'tooltip-edit-contact-' + contact.id}
-                              className="inline-block absolute bottom-[34px] whitespace-nowrap invisible opacity-0 z-10 py-2 px-3 text-xs font-medium text-white bg-neutral1 rounded-lg shadow-sm dark:bg-gray-700">
-                              Edit Contact
+                              className="cursor-pointer relative rounded-full p-1.5 bg-gray1 hover:bg-gray2 mr-2 flex items-center justify-center"
+                              onMouseEnter={() => {
+                                document
+                                  .querySelector('#tooltip-edit-contact-' + contact.id)
+                                  .classList.remove('invisible', 'opacity-0');
+                                document.querySelector('#edit-contact-icon-' + contact.id).classList.add('text-gray4');
+                                document
+                                  .querySelector('#edit-contact-icon-' + contact.id)
+                                  .classList.remove('text-gray3');
+                              }}
+                              onMouseLeave={() => {
+                                document
+                                  .querySelector('#tooltip-edit-contact-' + contact.id)
+                                  .classList.add('invisible', 'opacity-0');
+                                document.querySelector('#edit-contact-icon-' + contact.id).classList.add('text-gray3');
+                                document
+                                  .querySelector('#edit-contact-icon-' + contact.id)
+                                  .classList.remove('text-gray4');
+                              }}
+                              onClick={(e) => {
+                                e.stopPropagation();
+                                handleCardEdit(contact);
+                              }}>
+                              <Edit id={'edit-contact-icon-' + contact.id} className="text-gray3 w-4 h-4" />
+                              <div
+                                id={'tooltip-edit-contact-' + contact.id}
+                                className="inline-block absolute bottom-[34px] whitespace-nowrap invisible opacity-0 z-10 py-2 px-3 text-xs font-medium text-white bg-neutral1 rounded-lg shadow-sm dark:bg-gray-700">
+                                Edit Contact
+                              </div>
                             </div>
-                          </div>
-                          <div
-                            className="cursor-pointer relative rounded-full p-1.5 bg-gray1 hover:bg-gray2 mr-2 flex items-center justify-center"
-                            onMouseEnter={() => {
-                              document
-                                .querySelector('#tooltip-add-activity-' + contact.id)
-                                .classList.remove('invisible', 'opacity-0');
-                              document.querySelector('#add-activity-icon-' + contact.id).classList.add('text-gray4');
-                              document.querySelector('#add-activity-icon-' + contact.id).classList.remove('text-gray3');
-                            }}
-                            onMouseLeave={() => {
-                              document
-                                .querySelector('#tooltip-add-activity-' + contact.id)
-                                .classList.add('invisible', 'opacity-0');
-                              document.querySelector('#add-activity-icon-' + contact.id).classList.add('text-gray3');
-                              document.querySelector('#add-activity-icon-' + contact.id).classList.remove('text-gray4');
-                            }}
-                            // onClick={(e) => {
-                            //   e.stopPropagation();
-                            //   router.push({
-                            //     pathname: '/contacts/details',
-                            //     query: { id: contact.id, campaigns: true },
-                            //   });
-                            // }}
-                            onClick={(e) => {
-                              e.stopPropagation();
-                              handleAddActivity(contact);
-                            }}>
-                            <List id={'add-activity-icon-' + contact.id} className="text-gray3 w-4 h-4" />
                             <div
-                              id={'tooltip-add-activity-' + contact.id}
-                              role="tooltip"
-                              className="inline-block absolute bottom-[34px] whitespace-nowrap invisible z-10 py-2 px-3 text-xs font-medium text-white bg-neutral1 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
-                              Add Activity
+                              className="cursor-pointer relative rounded-full p-1.5 bg-gray1 hover:bg-gray2 mr-2 flex items-center justify-center"
+                              onMouseEnter={() => {
+                                document
+                                  .querySelector('#tooltip-add-activity-' + contact.id)
+                                  .classList.remove('invisible', 'opacity-0');
+                                document.querySelector('#add-activity-icon-' + contact.id).classList.add('text-gray4');
+                                document
+                                  .querySelector('#add-activity-icon-' + contact.id)
+                                  .classList.remove('text-gray3');
+                              }}
+                              onMouseLeave={() => {
+                                document
+                                  .querySelector('#tooltip-add-activity-' + contact.id)
+                                  .classList.add('invisible', 'opacity-0');
+                                document.querySelector('#add-activity-icon-' + contact.id).classList.add('text-gray3');
+                                document
+                                  .querySelector('#add-activity-icon-' + contact.id)
+                                  .classList.remove('text-gray4');
+                              }}
+                              // onClick={(e) => {
+                              //   e.stopPropagation();
+                              //   router.push({
+                              //     pathname: '/contacts/details',
+                              //     query: { id: contact.id, campaigns: true },
+                              //   });
+                              // }}
+                              onClick={(e) => {
+                                e.stopPropagation();
+                                handleAddActivity(contact);
+                              }}>
+                              <List id={'add-activity-icon-' + contact.id} className="text-gray3 w-4 h-4" />
+                              <div
+                                id={'tooltip-add-activity-' + contact.id}
+                                role="tooltip"
+                                className="inline-block absolute bottom-[34px] whitespace-nowrap invisible z-10 py-2 px-3 text-xs font-medium text-white bg-neutral1 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                                Add Activity
+                              </div>
                             </div>
-                          </div>
-                          {/* <div
+                            {/* <div
                             className="change-status relative cursor-pointer rounded-full p-1.5 bg-gray1 hover:bg-gray2 flex items-center justify-center group-hover"
                             onMouseEnter={() => {
                               document
@@ -1393,15 +1402,15 @@ const Table = ({
                               Change Status
                             </div>
                           </div> */}
-                        </div>
-                      </td>
-                    </tr>
-                  ))}
-              </>
-            ) : (
-              <></>
-            ),
-          )}
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
+                </>
+              ) : (
+                <></>
+              ),
+            )}
         </tbody>
         {addActivityPopup &&
           createPortal(
