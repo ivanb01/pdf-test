89b6d3cfb4a7bd5189a9919cdf664385ea7b6d85
Alberina <144018169+Alberina-opgny@users.noreply.github.com>
2023-10-04 21:11:17 +0200

    update import_source_text to import_source

--- src/components/overlays/add-contact/add-contact-manually/index.js
+++ src/components/overlays/add-contact/add-contact-manually/index.js
@@ -98,7 +98,7 @@ const AddContactManuallyOverlay = ({ handleClose, title }) => {
       last_name: '',
       email: '',
       phone_number: '',
-      import_source_text: 'Manually Added',
+      import_source: 'Manually Added',
       lead_source: '',
       tags: [],
     },
