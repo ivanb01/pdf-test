8a3ecc6851e213ea55036eb19436079b7a9ec6f2
Blendar Kabashi <blendar@opgny.com>
2023-10-10 13:48:15 -0400

    add packages locally

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,8 +6,8 @@ const Document = () => {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <script src="https://unpkg.com/flowbite@1.3.4/dist/datepicker.js"></script>
-        <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+        <script defer src="/js/datepicker-min.js"></script>
+        <script defer src="/js/lottie.js"></script>
         <meta name="viewport" content="width=device-width"></meta>
       </Head>
       <body>
