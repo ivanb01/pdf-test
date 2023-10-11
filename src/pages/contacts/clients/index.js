b363394c980ace22e4dbd92483cfc93b873e837f
blendar-opgny <blendar@opgny.com>
2023-10-11 14:18:09 -0400

    test dev

--- src/pages/contacts/clients/index.js
+++ src/pages/contacts/clients/index.js
@@ -173,10 +173,10 @@ const index = () => {
 
 export default index;
 
-export async function getServerSideProps(context) {
-  return {
-    props: {
-      requiresAuth: true,
-    },
-  };
-}
+// export async function getServerSideProps(context) {
+//   return {
+//     props: {
+//       requiresAuth: true,
+//     },
+//   };
+// }
