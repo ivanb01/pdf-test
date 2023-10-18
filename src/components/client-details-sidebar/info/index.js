b6f7b318d699774177223a909d4db6464c16048d
erzaopgny <142299390+erzaopgny@users.noreply.github.com>
2023-10-18 20:04:44 +0200

    fix lead source

--- src/components/client-details-sidebar/info/index.js
+++ src/components/client-details-sidebar/info/index.js
@@ -80,6 +80,7 @@ export default function Info({ client }) {
   }, [client]);
 
   const handleChangeSource = async (source) => {
+    console.log(source);
     try {
       console.log(source);
       await contactServices.updateContact(client.id, {
@@ -183,7 +184,8 @@ export default function Info({ client }) {
             options={leadSourceOptions}
             className="mt-3 mb-8"
             handleSelect={(source) => {
-              handleChangeSource(source.name);
+              console.log(source);
+              handleChangeSource(source.label);
             }}
             initialSelect={client?.lead_source}
             placeHolder={client?.lead_source ? client?.lead_source : 'Choose'}
--- src/components/overlays/edit-client/index.js
+++ src/components/overlays/edit-client/index.js
@@ -171,7 +171,7 @@ const EditContactOverlay = ({ className, handleClose, title, client }) => {
               activeIcon={false}
               options={leadSourceOptions}
               className="col-span-2"
-              handleSelect={(source) => (formik.values.lead_source = source.name)}
+              handleSelect={(source) => formik.setValues({ ...formik.values, lead_source: source.label })}
               initialSelect={formik.values.lead_source}
               placeHolder={formik.values.lead_source ? formik.values.lead_source : 'Choose'}
             />
