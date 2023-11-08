c5a9d8f794347dfc18375bbf2cc059b5f3518022
erzaopgny <142299390+erzaopgny@users.noreply.github.com>
2023-11-08 23:13:04 +0300

    remove menu flicker

--- src/components/shared/SubMenuCard/index.js
+++ src/components/shared/SubMenuCard/index.js
@@ -1,22 +1,19 @@
 import React from 'react';
-import * as Tooltip from '@radix-ui/react-tooltip';
-
+import * as HoverCard from '@radix-ui/react-hover-card';
 const SubMenuContent = ({ triggerElement, children, side, align, open, style, className }) => {
   return (
-    <Tooltip.Provider>
-      <Tooltip.Root open={open} delayDuration={0}>
-        <Tooltip.Trigger asChild>{triggerElement}</Tooltip.Trigger>
-        <Tooltip.Portal>
-          <Tooltip.Content
-            style={{ zIndex: 999999, ...style }}
-            className={!children ? '' : `TooltipContent text-white  ${className}`}
-            side={side}
-            align={align}>
-            {children}
-          </Tooltip.Content>
-        </Tooltip.Portal>
-      </Tooltip.Root>
-    </Tooltip.Provider>
+    <HoverCard.Root open={open} openDelay={0} closeDelay={0}>
+      <HoverCard.Trigger asChild>{triggerElement}</HoverCard.Trigger>
+      <HoverCard.Portal>
+        <HoverCard.Content
+          style={{ zIndex: 999999, ...style }}
+          className={!children ? '' : ` text-white ${className}`}
+          side={side}
+          align={align}>
+          {children && children}
+        </HoverCard.Content>
+      </HoverCard.Portal>
+    </HoverCard.Root>
   );
 };
 
