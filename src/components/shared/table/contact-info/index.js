7b4fb57543f2023d5ccc066a8d54d479d96e080a
erzaopgny <142299390+erzaopgny@users.noreply.github.com>
2023-11-10 10:50:09 +0300

    change layout of contact info component when in categorization

--- src/components/shared/table/contact-info/index.js
+++ src/components/shared/table/contact-info/index.js
@@ -37,7 +37,13 @@ const ContactInfo = ({ data, handleSelect, handleAction, showAIChip, inCategoriz
       return {
         name: 'Contact Added Manually',
         icon: (
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            style={{ marginTop: '-7px' }}>
             <path
               d="M6.04175 13.9584H11.4584V12.7084H6.04175V13.9584ZM6.04175 10.625H13.9584V9.37508H6.04175V10.625ZM6.04175 7.29171H13.9584V6.04175H6.04175V7.29171ZM4.42316 17.0834C4.00222 17.0834 3.64591 16.9375 3.35425 16.6459C3.06258 16.3542 2.91675 15.9979 2.91675 15.577V4.42317C2.91675 4.00222 3.06258 3.64591 3.35425 3.35425C3.64591 3.06258 4.00222 2.91675 4.42316 2.91675H15.577C15.9979 2.91675 16.3542 3.06258 16.6459 3.35425C16.9375 3.64591 17.0834 4.00222 17.0834 4.42317V15.577C17.0834 15.9979 16.9375 16.3542 16.6459 16.6459C16.3542 16.9375 15.9979 17.0834 15.577 17.0834H4.42316Z"
               fill="#9CA3AF"
@@ -48,7 +54,7 @@ const ContactInfo = ({ data, handleSelect, handleAction, showAIChip, inCategoriz
     } else if (source === 'Google Contacts') {
       return {
         name: 'Google Contact',
-        icon: <Image src={GoogleContact} height={20} width={20} />,
+        icon: <Image src={GoogleContact} height={17} width={17} style={{ marginTop: '-6px' }} />,
       };
     } else {
       return <></>;
@@ -57,6 +63,35 @@ const ContactInfo = ({ data, handleSelect, handleAction, showAIChip, inCategoriz
   return (
     <div className="flex items-center relative">
       {handleSelect && <Input type="checkbox" className="mr-1" onChange={(e) => handleSelect(e, data)}></Input>}
+      {inCategorization && (
+        <div className={'flex items-center gap-1.5 flex-shrink-0 mr-3'}>
+          <div>
+            <div className={'h-2'}>{getSource(data.import_source_text).icon}</div>
+          </div>
+          {data.summary !== null ? (
+            <TooltipComponent
+              side={'top'}
+              align={'center'}
+              triggerElement={
+                <InfoSharpIcon
+                  className={`text-gray3 hover:text-gray4 mt-1`}
+                  style={{ height: '15px', width: '15px' }}
+                  aria-hidden="true"
+                />
+              }>
+              <div className={`max-w-[260px] pointer-events-none text-white bg-neutral1 rounded-lg`}>
+                <div className={`flex gap-1.5`}>
+                  {getSource(data.import_source_text).icon}
+                  <p className={'text-xs leading-4 font-medium'}>{getSource(data.import_source_text).name}</p>
+                </div>
+                <p className="text-xs leading-4 font-normal">{data.summary}</p>
+              </div>
+            </TooltipComponent>
+          ) : (
+            <></>
+          )}
+        </div>
+      )}
       <div className="h-10 w-10 flex-shrink-0 bg-gray-500 rounded-full">
         {data.image && data.image !== null ? (
           <img className="h-10 w-10 flex-shrink-0 rounded-full bg-gray-400" src={data.image} />
@@ -94,35 +129,6 @@ const ContactInfo = ({ data, handleSelect, handleAction, showAIChip, inCategoriz
               </div> */}
         </div>
       </div>
-      {inCategorization && (
-        <div className={'flex items-center gap-1.5 flex-shrink-0 ml-6'}>
-          <div>
-            <div className={'h-5'}>{getSource(data.import_source_text).icon}</div>
-          </div>
-          {data.summary !== null ? (
-            <TooltipComponent
-              side={'right'}
-              align={'center'}
-              triggerElement={
-                <InfoSharpIcon
-                  className={`text-gray3 hover:text-gray4 mb-1.5`}
-                  style={{ height: '18px', width: '18px' }}
-                  aria-hidden="true"
-                />
-              }>
-              <div className={`w-[260px] pointer-events-none text-white bg-neutral1 rounded-lg`}>
-                <div className={`flex gap-1.5`}>
-                  {getSource(data.import_source_text).icon}
-                  <p className={'text-xs leading-4 font-medium'}>{getSource(data.import_source_text).name}</p>
-                </div>
-                <p className="text-xs leading-4 font-normal">{data.summary}</p>
-              </div>
-            </TooltipComponent>
-          ) : (
-            <></>
-          )}
-        </div>
-      )}
       {handleAction && (
         <div>
           {data.assigned == 2 ? null : data.assigned == 1 ? (
