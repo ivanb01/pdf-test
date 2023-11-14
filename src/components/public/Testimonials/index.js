d0e4cabb03af6160b0608faef037cc973d5c4762
blendar-opgny <blendar@opgny.com>
2023-11-14 14:18:44 +0100

    fix images

--- src/components/public/Testimonials/index.js
+++ src/components/public/Testimonials/index.js
@@ -14,69 +14,74 @@ import Image from 'next/image';
 
 const testimonials = [
   {
-    description: 'Oneline has helped me follow up with clients, I didn&apos;t even know were still engaged. Its a true end to end solution for agents and brokerages. Our clients, agents and our brokerage has benefited from signing up.',
+    description:
+      'Oneline has helped me follow up with clients, I didn&apos;t even know were still engaged. Its a true end to end solution for agents and brokerages. Our clients, agents and our brokerage has benefited from signing up.',
     logo: levelLogo,
     name: 'John Doe',
     position: 'CEO, LEVEL',
-    avatar: testimonialsAvatar
+    avatar: testimonialsAvatar,
   },
   {
-    description: 'We are a brokerage partner and use Oneline to run our entire operation. From CRM, to agent management, agent payments and deal management to website, marketing and support. Its the only product we have seen that provides everything.',
+    description:
+      'We are a brokerage partner and use Oneline to run our entire operation. From CRM, to agent management, agent payments and deal management to website, marketing and support. Its the only product we have seen that provides everything.',
     logo: spireLogo,
     name: 'Joseph Rodriguez',
     position: 'CEO, LEVEL',
-    avatar: testimonialsAvatar
-  }
+    avatar: testimonialsAvatar,
+  },
 ];
 
 export const Testimonials = () => {
-  return <div className={styles['section-main-container']}>
-    <Image src={testimonialsTop} alt="top-arc"/>
-    <div className={styles['section-main-subcontainer']}>
-      <div className={styles['section-container']}>
-        <Swiper
-          modules={[Pagination]}
-          speed={750}
-          slidesPerView={1}
-          pagination={{ clickable: true }}
-          breakpoints={{
-            1024: {
-              slidesPerView: 2
-            }
-          }}
-        >
-          {
-            testimonials.map((testimonial, i) => (
-                <SwiperSlide key={'slide' + i}>
-                  <div className={i %2 === 0 ? styles['section-swiper-container-left'] : styles['section-swiper-container-right']}>
-                    <div className={styles['section-swiper-container-content']}>
-                      <div className={styles['section-swiper-container-content-logo']}>
-                        <Image width="78" src={testimonial.logo} alt="logo"/>
-                      </div>
-                      <div className={styles['section-swiper-container-content-container']}>
-                        <Image
-                          className={styles['section-swiper-container-content-container-quotation']}
-                          src={quote} alt="quote"/>
-                        <p className={styles['section-swiper-container-content-container-text']}>
-                          {testimonial.description}
-                        </p>
-                      </div>
+  return (
+    <div className={styles['section-main-container']}>
+      <Image src={testimonialsTop} alt="top-arc" className="w-full" />
+      <div className={styles['section-main-subcontainer']}>
+        <div className={styles['section-container']}>
+          <Swiper
+            modules={[Pagination]}
+            speed={750}
+            slidesPerView={1}
+            pagination={{ clickable: true }}
+            breakpoints={{
+              1024: {
+                slidesPerView: 2,
+              },
+            }}>
+            {testimonials.map((testimonial, i) => (
+              <SwiperSlide key={'slide' + i}>
+                <div
+                  className={
+                    i % 2 === 0 ? styles['section-swiper-container-left'] : styles['section-swiper-container-right']
+                  }>
+                  <div className={styles['section-swiper-container-content']}>
+                    <div className={styles['section-swiper-container-content-logo']}>
+                      <Image width="78" src={testimonial.logo} alt="logo" />
                     </div>
-                    <div className={styles['section-swiper-container-person']}>
-                      <Image src={testimonial.avatar} alt="profile img"/>
-                      <div className={styles['section-swiper-container-person-info']}>
-                        <p className={styles['section-swiper-container-person-info-name']}>{testimonial.name}</p>
-                        <p className={styles['section-swiper-container-person-info-position']}>{testimonial.position}</p>
-                      </div>
+                    <div className={styles['section-swiper-container-content-container']}>
+                      <Image
+                        className={styles['section-swiper-container-content-container-quotation']}
+                        src={quote}
+                        alt="quote"
+                      />
+                      <p className={styles['section-swiper-container-content-container-text']}>
+                        {testimonial.description}
+                      </p>
                     </div>
                   </div>
-                </SwiperSlide>
-              )
-            )
-          }
-        </Swiper>
+                  <div className={styles['section-swiper-container-person']}>
+                    <Image src={testimonial.avatar} alt="profile img" />
+                    <div className={styles['section-swiper-container-person-info']}>
+                      <p className={styles['section-swiper-container-person-info-name']}>{testimonial.name}</p>
+                      <p className={styles['section-swiper-container-person-info-position']}>{testimonial.position}</p>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
       </div>
+      <Image src={testimonialsBottom} alt="bottom-arc" className="w-full" />
     </div>
-    <Image src={testimonialsBottom} alt="bottom-arc"/>
-  </div>;
+  );
 };
