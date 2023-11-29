f2c819912661d38770a2cf7ba90b6595dfcf86d2
erzaopgny <142299390+erzaopgny@users.noreply.github.com>
2023-11-29 16:20:25 +0100

    make changes at importing contacts table

--- src/components/shared/table/index.js
+++ src/components/shared/table/index.js
@@ -1630,6 +1630,7 @@ const Table = ({
           </tr>
         </thead>
         <tbody className=" bg-white">
+          {console.log(data, 'data')}
           {!data.length ? (
             <tr className="h-[233px] text-center align-middle">
               <td className="text-center align-middle text-gray-400 text-sm italic">
@@ -1641,31 +1642,25 @@ const Table = ({
               <tr key={i} className="border-b border-gray-200">
                 <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
                   <div className="flex items-center justify-between">
-                    {dataItem.details ? (
-                      <div className="flex items-center relative">
-                        <div className="font-medium text-gray7">{dataItem.details}</div>
-                      </div>
-                    ) : (
-                      <div className="flex items-center relative">
-                        <div className="h-10 w-10 flex-shrink-0 bg-gray-500 rounded-full">
-                          {dataItem.image && dataItem.image !== null ? (
-                            <img className="h-10 w-10 flex-shrink-0 rounded-full bg-gray-400" src={dataItem.image} />
-                          ) : (
-                            <span className="inline-flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
-                              <span className="text-sm font-medium leading-none sss text-white">
-                                {getInitials(dataItem.name).toUpperCase()}
-                              </span>
+                    <div className="flex items-center relative">
+                      <div className="h-10 w-10 flex-shrink-0 bg-gray-500 rounded-full">
+                        {dataItem?.image && dataItem?.image !== null ? (
+                          <img className="h-10 w-10 flex-shrink-0 rounded-full bg-gray-400" src={dataItem?.image} />
+                        ) : (
+                          <span className="inline-flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+                            <span className="text-sm font-medium leading-none sss text-white">
+                              {getInitials(dataItem?.name).toUpperCase()}
                             </span>
-                          )}
-                        </div>
-                        <div className=" flex flex-col">
-                          <div className={`font-medium text-gray7 flex`}>
-                            <p>{dataItem?.name}</p>
-                          </div>
-                          <p className={'text-gray-500 font-medium'}>{dataItem?.email}</p>
-                        </div>
+                          </span>
+                        )}
                       </div>
-                    )}
+                      <div className=" flex flex-col">
+                        <div className={`font-medium text-gray7 flex`}>
+                          <p>{dataItem?.name}</p>
+                        </div>
+                        <p className={'text-gray-500 font-medium'}>{dataItem?.email}</p>
+                      </div>
+                    </div>
                     {tableFor == 'import-google-contacts-failed' && (
                       <div className="flex items-center justify-center">
                         <Error className="h-5 w-5 text-red4 mr-2" />
