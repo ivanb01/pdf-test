13087451d81ea07d4b123fa1f522e1bf152c0fa6
blendar-opgny <blendar@opgny.com>
2023-12-04 10:04:40 +0100

    bugfix

--- src/components/withAuth.js
+++ src/components/withAuth.js
@@ -13,7 +13,7 @@ export default function withAuth(Component) {
 
         let maintenance = process.env.NEXT_PUBLIC_MAINTENANCE_MODE;
         console.log(maintenance);
-        if (maintenance === 'true') {
+        if (maintenance === 'true' || maintenance === true) {
           router.push('/maintenance');
         }
         if (!isAuthenticated) {
