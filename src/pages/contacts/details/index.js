f79c0e1daf2d4a5d3ddb0fa0fc059873124c1589
erzaopgny <142299390+erzaopgny@users.noreply.github.com>
2024-01-10 23:01:51 +0100

    add Start to communicate button in Clients details

--- src/pages/contacts/details/index.js
+++ src/pages/contacts/details/index.js
@@ -6,46 +6,36 @@ import communication from '/public/images/communication.svg';
 import SimpleBar from 'simplebar-react';
 import call from '/public/images/call-icon.svg';
 import edit from '/public/images/edit-icon.svg';
-import email from '/public/images/email-icon.svg';
-import sms from '/public/images/sms-icon.svg';
-import whatsapp from '/public/images/whatsapp-icon.svg';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
 import addNote from '/public/images/add-note.svg';
 import Button from '@components/shared/button';
 import DateChip from '@components/shared/chip/date-chip';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { findTagsOption, formatDateLL } from '@global/functions';
-import Visibility from '@mui/icons-material/Visibility';
-import SwitchComponent from '@components/Switch';
 import { Switch } from '@headlessui/react';
 import { useRouter } from 'next/router';
 import { useDispatch, useSelector } from 'react-redux';
 import PropertiesSection from '@components/PropertiesSection';
 import Loader from '@components/shared/loader';
-import { addContactNote, deleteContactNote, getContactActivities, getContactNotes } from '@api/contacts';
-import Add from '@mui/icons-material/Add';
-import TextArea from '@components/shared/textarea';
-import { useFormik } from 'formik';
-import Overlay from '@components/shared/overlay';
+import { deleteContactNote, getContactActivities, getContactNotes } from '@api/contacts';
 import toast from 'react-hot-toast';
 import ReviewContact from '@components/overlays/review-contact';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
 import Feeds from '@components/shared/feeds';
 import FilterDropdown from '@components/shared/dropdown/FilterDropdown';
 import { Delete, Edit, More } from '@mui/icons-material';
 import Text from '@components/shared/text';
 import MoreVert from '@mui/icons-material/MoreVert';
 import NoteModal from '@components/overlays/note-modal';
+import { createPortal } from 'react-dom';
+import CommunicationForm from '@components/overlays/communication-form';
 
 const index = () => {
   const router = useRouter();
-  const dispatch = useDispatch();
   const { id } = router.query;
 
   const [noteId, setNoteId] = useState(0);
   const [addNoteModal, setAddNoteModal] = useState(false);
   const [editNoteModal, setEditNoteModal] = useState(false);
-  const refetchPart = useSelector((state) => state.global.refetchPart);
-  const refetchData = useSelector((state) => state.global.refetchData);
   const contacts = useSelector((state) => state.contacts.allContacts.data);
   const [contact, setContact] = useState(null);
   const [notes, setNotes] = useState([]);
@@ -136,6 +126,7 @@ const index = () => {
   const handleEditActivity = () => {
     console.log('edit activity');
   };
+  const [openCommunicationPopup, setOpenCommunicationPopup] = useState(false);
 
   const handleDeleteActivity = () => {
     console.log('delete activity');
@@ -376,43 +367,19 @@ const index = () => {
 
             <div className="flex-grow md:mx-3 order-last md:order-2">
               <div className="bg-white px-3 md:px-6 py-[20px] client-details-box-shadow rounded-lg mb-3">
-                <div className="flex items-center">
-                  <img src={communication.src} />
-                  <div className="text-gray8 ml-[6px] text-sm font-semibold">All Communication</div>
+                <div className="flex items-center justify-between">
+                  <div className={'flex items-center'}>
+                    <img src={communication.src} />
+                    <div className="text-gray8 ml-[6px] text-sm font-semibold">All Communication</div>
+                  </div>
+                  <button
+                    onClick={() => setOpenCommunicationPopup(true)}
+                    className="flex justify-center items-center gap-2 py-2 px-[14px] rounded-full bg-lightBlue1  hover:bg-lightBlue2">
+                    <ChatBubbleOutlineOutlinedIcon className={'h-[18px] w-[18px] text-lightBlue5'} />
+                    <span className={'text-sm font-semibold leading-5 text-lightBlue6'}>Start communicate</span>
+                  </button>
                 </div>
                 <Feeds contactId={id} activities={activities} setActivities={setActivities} />
-                <div className="mt-6">
-                  <div className="text-gray8 text-sm font-semibold mb-2">Start to communicate via:</div>
-                  <div className="grid md:grid-cols-3 grid-cols-1">
-                    <div className="border-gray1 border mb-[10px] md:mb-0 mr-0 md:mr-[10px] client-details-box-shadow rounded-lg px-4 py-3 flex justify-between">
-                      <div>
-                        <img src={whatsapp.src} />
-                        <div className="text-sm text-gray7 ml-1 font-semibold mt-1">Whatsapp</div>
-                      </div>
-                      <Button secondary className="!rounded-[222px] !font-semibold !px-3 !py-2 self-end">
-                        Start Chat
-                      </Button>
-                    </div>
-                    <div className="border-gray1 border mb-[10px] md:mb-0 mr-0 md:mr-[10px] client-details-box-shadow rounded-lg px-4 py-3 flex justify-between">
-                      <div>
-                        <img src={sms.src} />
-                        <div className="text-sm text-gray7 ml-1 font-semibold mt-1">SMS</div>
-                      </div>
-                      <Button secondary className="!rounded-[222px] !font-semibold !px-3 !py-2 self-end">
-                        Send SMS
-                      </Button>
-                    </div>
-                    <div className="border-gray1 border client-details-box-shadow rounded-lg px-4 py-3 flex justify-between">
-                      <div>
-                        <img src={email.src} />
-                        <div className="text-sm text-gray7 ml-1 font-semibold mt-1">Email</div>
-                      </div>
-                      <Button secondary className="!rounded-[222px] !font-semibold !px-3 !py-2 self-end">
-                        Send Email
-                      </Button>
-                    </div>
-                  </div>
-                </div>
               </div>
 
               <div className="bg-white px-3 md:px-6 py-[20px] client-details-box-shadow rounded-lg">
@@ -482,6 +449,11 @@ const index = () => {
       {editNoteModal && (
         <NoteModal setNotes={setNotes} note={noteToEdit} id={id} handleCloseModal={() => setEditNoteModal(false)} />
       )}
+      {openCommunicationPopup &&
+        createPortal(
+          <CommunicationForm handleCloseOverlay={() => setOpenCommunicationPopup(false)} client={contact} />,
+          document.getElementById('modal-portal'),
+        )}
       {editingContact && (
         <ReviewContact handleClose={() => setEditingContact(false)} client={contact} title="Edit Contact" />
       )}
