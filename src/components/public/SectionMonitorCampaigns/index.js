4a6d1a3ac752fd17e7eebcad8f92188f6144005b
erzaopgny <142299390+erzaopgny@users.noreply.github.com>
2024-01-18 12:13:08 +0100

    F/erza/review contact chips (#198)
    * make entire row clickable in NEIGHBORHOOD dropdown
    
    * fix chips in review popup
    
    * fix chips in review popup
    
    * fix images height and width in website

--- src/components/public/SectionMonitorCampaigns/index.js
+++ src/components/public/SectionMonitorCampaigns/index.js
@@ -10,7 +10,6 @@ import campaignReport2 from '/public/images/public/campaign-reports-2.png';
 import createCampaign1 from '/public/images/public/create-campaign-1.png';
 import createCampaign2 from '/public/images/public/create-campaign-2.png';
 
-
 const SectionMonitorCampaigns = () => {
   return (
     <div className={styles.section}>
@@ -25,22 +24,43 @@ const SectionMonitorCampaigns = () => {
           </div>
           <div className={styles['section__middle-wrapper']}>
             <div className={styles['section__images-run-campaign']}>
-              <Image src={runCampaign1} alt="run campaign image title"/>
-              <Image src={runCampaign2} alt="run campaign image" />
+              <Image
+                src={runCampaign1}
+                alt="run campaign image title"
+                style={{ maxHeight: '28px', maxWidth: '192px' }}
+              />
+              <Image src={runCampaign2} alt="run campaign image" style={{ maxHeight: '189px', maxWidth: '347px' }} />
             </div>
             <div className={styles['section__images-campaign-reports']}>
-              <Image src={campaignReport1} alt="campaign reports image title"/>
-              <Image src={campaignReport2} alt="campaign reports image" />
+              <Image
+                src={campaignReport1}
+                className={styles['section__images-campaign-reports__titlee']}
+                alt="campaign reports image title"
+                style={{ maxWidth: '202px', maxHeight: '28px' }}
+              />
+              <Image
+                src={campaignReport2}
+                alt="campaign reports image"
+                style={{ maxWidth: '268px', maxHeight: '120px' }}
+              />
             </div>
           </div>
           <div className={styles['section__images-create-campaign']}>
-            <Image src={createCampaign1} alt="create campaign image title"/>
-            <Image src={createCampaign2} alt="create campaign image" />
+            <Image
+              src={createCampaign1}
+              alt="create campaign image title"
+              style={{ maxWidth: '225px', maxHeight: '28px' }}
+            />
+            <Image
+              src={createCampaign2}
+              alt="create campaign image"
+              style={{ maxWidth: '233px', maxHeight: '524px' }}
+            />
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default SectionMonitorCampaigns;
