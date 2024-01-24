222afb8ed1667b0f77402b4fea323b95889a934a
blendar-opgny <blendar@opgny.com>
2024-01-24 10:34:52 +0100

    remove packages

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,10 +6,8 @@ const Document = () => {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet"></link>
         <script defer src="/js/datepicker-min.js"></script>
         <script defer src="/js/lottie.js"></script>
-        <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
       </Head>
       <body>
         <Main />
