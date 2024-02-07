749e7c86cd545fdfecd4569609de0dbb0d98e406
erzaopgny <142299390+erzaopgny@users.noreply.github.com>
2024-02-07 12:30:25 +0100

    add get started button (#201)

--- src/components/public/SectionIntro/index.js
+++ src/components/public/SectionIntro/index.js
@@ -2,8 +2,8 @@
 import styles from './styles.module.scss';
 import clsx from 'clsx';
 import Image from 'next/image';
-import {Input} from 'components/public/Input';
-import {Button} from 'components/public/Button';
+import { Input } from 'components/public/Input';
+import { Button } from 'components/public/Button';
 import oxfordLogo from '/public/images/public/oxford-logo-dark.svg';
 import spireLogo from '/public/images/public/spire-logo-dark.svg';
 import levelLogo from '/public/images/public/level-logo-dark.svg';
@@ -12,9 +12,10 @@ import introLeft from '/public/images/public/intro-left-bg.png';
 import introRight from '/public/images/public/intro-right-bg.png';
 import intoMobileRight from '/public/images/public/intro-right-bg-mobile.png';
 import bgSm from '/public/images/public/intro-mobile-bg.png';
+import { useRouter } from 'next/router';
 
 export const SectionIntro = () => {
-
+  const router = useRouter();
   const redirect = () => {
     //router.push('/contact')
   };
@@ -44,24 +45,23 @@ export const SectionIntro = () => {
               Oneline helps you close more deals, keeps you on top of your clients and relationships & automates all
               your workflows as an agent & brokerage.
             </p>
-            <form>
-              <Input placeholder="Enter your email" onChange={(e) => console.log(e.target.value)}/>
-              <Button type="primary">
-                Start Free Trial
+            <div style={{ alignSelf: 'flex-start', padding: '15px 0px' }}>
+              <Button type="primary" onClick={() => router.push('/authentication/sign-up')}>
+                GET STARTED
               </Button>
-            </form>
+            </div>
             <p className={styles['section__content-left-terms']}>
-              Get Notified when we’re launching and start your free trial, no credit card necessary.
-              By providing your email, you agree to our <a href="#">terms of service</a>.
+              Get Notified when we’re launching and start your free trial, no credit card necessary. By providing your
+              email, you agree to our <a href="#">terms of service</a>.
             </p>
             <div className={styles['section__content-left-logos']}>
-              <Image src={oxfordLogo} alt="oxford logo"/>
-              <Image src={spireLogo} alt="spire logo"/>
-              <Image src={levelLogo} alt="level logo"/>
+              <Image src={oxfordLogo} alt="oxford logo" />
+              <Image src={spireLogo} alt="spire logo" />
+              <Image src={levelLogo} alt="level logo" />
             </div>
           </div>
           <div className={styles['section__content-right']}>
-            <Image src={intoChatBg} alt="intro chat bg"/>
+            <Image src={intoChatBg} alt="intro chat bg" />
           </div>
         </div>
         <div className={clsx(styles['section__content'], styles['section__content--sm'])}>
@@ -69,8 +69,7 @@ export const SectionIntro = () => {
             <h1>
               Faster.Better.Easier.
               <span>
-                All in{' '}
-                <span className={styles.fill}>ONE</span>
+                All in <span className={styles.fill}>ONE</span>
                 <span>LINE</span>
               </span>
             </h1>
@@ -78,24 +77,25 @@ export const SectionIntro = () => {
               Oneline helps you close more deals, keeps you on top of your clients and relationships & automates all
               your workflows as an agent & brokerage.
             </p>
-            <div className={styles['section__content-right']} style={{backgroundImage: `url(${intoMobileRight.src})`}}>
-              <Image src={intoChatBg} alt="intro chat bg"/>
+            <div style={{ display: 'flex', justifyContent: 'flex-start', marginBottom: '20px' }}>
+              <Button type="primary" onClick={() => router.push('/authentication/sign-up')}>
+                GET STARTED
+              </Button>
             </div>
-            <div className={styles['section__content-bg-sm']} style={{backgroundImage: `url(${bgSm.src})`}}>
-              <form>
-                <Input placeholder="Enter your email" onChange={(e) => console.log(e.target.value)}/>
-                <Button type="primary">
-                  Start Free Trial
-                </Button>
-              </form>
+            <div
+              className={styles['section__content-right']}
+              style={{ backgroundImage: `url(${intoMobileRight.src})` }}>
+              <Image src={intoChatBg} alt="intro chat bg" />
+            </div>
+            <div className={styles['section__content-bg-sm']} style={{ backgroundImage: `url(${bgSm.src})` }}>
               <p className={styles['section__content-left-terms']}>
-                Get Notified when we’re launching and start your free trial, no credit card necessary.
-                By providing your email, you agree to our <a href="#">terms of service</a>.
+                Get Notified when we’re launching and start your free trial, no credit card necessary. By providing your
+                email, you agree to our <a href="#">terms of service</a>.
               </p>
               <div className={styles['section__content-left-logos']}>
-                <Image src={oxfordLogo} alt="oxford logo"/>
-                <Image src={spireLogo} alt="spire logo"/>
-                <Image src={levelLogo} alt="level logo"/>
+                <Image src={oxfordLogo} alt="oxford logo" />
+                <Image src={spireLogo} alt="spire logo" />
+                <Image src={levelLogo} alt="level logo" />
               </div>
             </div>
           </div>
