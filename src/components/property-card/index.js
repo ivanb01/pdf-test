4a8322e1b15ca23a4a2242df6ec6c794991e096c
blendar-opgny <blendar@opgny.com>
2024-03-21 14:23:41 +0100

    fix client thoughts

--- src/components/property-card/index.js
+++ src/components/property-card/index.js
@@ -125,14 +125,18 @@ const PropertyCard = ({
           } rounded-full h-fit px-2 py-1 text-[10px] font-medium`}>
           {property?.STATUS}
         </div>
-        <div className={'flex gap-2 absolute top-2 right-3 justify-center'}>
-          <div
-            role={'button'}
-            onClick={() => setOpenFeedbackModal(true)}
-            className={` rounded-full border border-gray-300 bg-[#ffffffe8] text-gray5 h-fit px-2 py-1 text-[10px] font-medium flex items-center justify-center cursor-pointer`}>
-            Client’s thoughts
+        {deletePropertyFromPortfolio && (
+          <div className={'flex gap-2 absolute top-2 right-3 justify-center'}>
+            {clientNote && clientNote.length > 0 && (
+              <div
+                role={'button'}
+                onClick={() => setOpenFeedbackModal(true)}
+                className={` rounded-full border border-gray-300 bg-[#ffffffe8] text-gray5 h-fit px-2 py-1 text-[10px] font-medium flex items-center justify-center cursor-pointer`}>
+                Client’s thoughts
+              </div>
+            )}
           </div>
-        </div>
+        )}
         {!putFeedback && (
           <div className={'flex gap-2'}>
             <TooltipComponent
