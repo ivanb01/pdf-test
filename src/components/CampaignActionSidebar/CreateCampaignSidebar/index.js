88de6dce0cc5d1c93f2fe1a1d3fe715fecd9be03
blendar-opgny <82260563+blendar-opgny@users.noreply.github.com>
2024-04-18 13:55:31 +0200

    bugfix (#310)

--- src/components/CampaignActionSidebar/CreateCampaignSidebar/index.js
+++ src/components/CampaignActionSidebar/CreateCampaignSidebar/index.js
@@ -612,7 +612,15 @@ const CreateCampaignSidebar = ({ open, setOpen }) => {
           </SimpleBar>
 
           <div className="z-50 sticky left-0 right-0 bottom-0 bg-white px-6 py-4 flex justify-end border-t border-gray1">
-            <Button label="Cancel" white className="" onClick={() => setOpen(false)} />
+            <Button
+              label="Cancel"
+              white
+              className=""
+              onClick={() => {
+                resetCreateCampaign();
+                setOpen(false);
+              }}
+            />
             <a
               onClick={() => addNewEvent()}
               className="mx-3 px-[14px] py-[8px] rounded-[222px] border-2 bg-lightBlue1 border-lightBlue3 cursor-pointer text-lightBlue3 text-sm font-semibold">
