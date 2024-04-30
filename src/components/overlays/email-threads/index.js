9ca9afe5e8fb31c6a647fc62b791f83f08e412de
erzaopgny <142299390+erzaopgny@users.noreply.github.com>
2024-04-30 11:59:53 +0200

    B/erza/agent name in thread (#371)
    * remove sms icon from preview of template
    
    * fix agent name and surname in inbox thread

--- src/components/overlays/email-threads/index.js
+++ src/components/overlays/email-threads/index.js
@@ -9,26 +9,29 @@ import { useSelector } from 'react-redux';
 import DOMPurify from 'dompurify';
 
 const EmailItem = ({
-  name,
-  isLast,
-  body,
-  message_header_id,
-  sentDate,
-  threadId,
-  contactEmail,
-  setInboxData,
-  fromEmail,
-  inboxData,
-  subject,
-  email,
-  openedEditor,
-  setOpenedEditor,
-  setHideTopButton,
-}) => {
+                     name,
+                     isLast,
+                     body,
+                     message_header_id,
+                     sentDate,
+                     threadId,
+                     contactEmail,
+                     setInboxData,
+                     fromEmail,
+                     inboxData,
+                     subject,
+                     email,
+                     openedEditor,
+                     setOpenedEditor,
+                     setHideTopButton,
+                   }) => {
   const [message, setMessage] = useState('');
   const [loading, setLoading] = useState(false);
   const allContacts = useSelector((state) => state.contacts.allContacts);
   const userInfo = useSelector((state) => state.global.userInfo);
+  useEffect(() => {
+    console.log(name, 'name', email, userInfo?.email);
+  }, [name]);
   const _replyInThread = () => {
     setLoading(true);
     setInboxData({
@@ -40,6 +43,8 @@ const EmailItem = ({
           message_header_id: message_header_id,
           thread_id: threadId,
           sent_date: new Date(),
+          from_first_name: userInfo?.first_name,
+          from_last_name: userInfo?.last_name,
         },
         ...inboxData[threadId],
       ],
@@ -80,7 +85,7 @@ const EmailItem = ({
                 ? userInfo?.first_name + ' ' + userInfo?.last_name
                 : name.replace(/\bundefined\b/g, '')}
             </h5>
-            <div className="text-[#475467] font-medium text-sm">{sentDate}</div>
+            <div className='text-[#475467] font-medium text-sm'>{sentDate}</div>
           </div>
           <div className={'text-sm font-normal'} dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(body) }}></div>
         </div>
@@ -91,9 +96,9 @@ const EmailItem = ({
             <>
               <RichtextEditor
                 height={200}
-                label="Message"
+                label='Message'
                 value={message}
-                placeholder="Write message here..."
+                placeholder='Write message here...'
                 onContentChange={(value) => setMessage(value)}
               />
               <Button
@@ -120,18 +125,19 @@ const EmailsPopup = ({ handleClose, threadData, setInboxData, contactEmail, inbo
   const [showAll, setShowAll] = useState(false);
   const [openedEditor, setOpenedEditor] = useState(false);
   const [hideTopButton, setHideTopButton] = useState(false);
+  const userInfo = useSelector((state) => state.global.userInfo);
 
   return (
     <Overlay
       alignStart
-      className=" w-[792px]"
+      className=' w-[792px]'
       titleButton={
         !hideTopButton && (
           <Button
             primary
-            className="ml-4 mr-2"
-            label="Reply"
-            size="small"
+            className='ml-4 mr-2'
+            label='Reply'
+            size='small'
             onClick={() => {
               let element = document.querySelector('.email-area');
               setOpenedEditor(true);
@@ -147,7 +153,7 @@ const EmailsPopup = ({ handleClose, threadData, setInboxData, contactEmail, inbo
       title={threadData[0]?.subject?.length > 0 ? threadData[0]?.subject : '(no subject)'}>
       {threadData?.length > 3 && !showAll ? (
         <div
-          className="email-area"
+          className='email-area'
           style={{ height: 'calc(100% - 78px)', maxHeight: 'calc(100% - 78px) ', overflow: 'auto' }}>
           <div className={'pt-[18px] pb-[36px] '}>
             <EmailItem
@@ -159,9 +165,9 @@ const EmailsPopup = ({ handleClose, threadData, setInboxData, contactEmail, inbo
               threadId={threadData[0]?.thread_id}
               message_header_id={threadData[0]?.message_header_id}
               sentDate={timeAgo(threadData[0]?.sent_date)}
-              name={`${threadData[threadData[threadData?.length - 1]]?.from_first_name}  ${threadData[threadData[threadData?.length - 1]]?.from_last_name}`}
+              name={`${threadData[0]?.from_first_name}  ${threadData[0]?.from_last_name}`}
               body={threadData[0]?.html_body?.length > 0 ? threadData[0]?.html_body : threadData[0]?.body}
-              email={threadData[threadData?.length - 1]?.from_email}
+              email={threadData[0]?.from_email ?? userInfo?.email}
               openedEditor={openedEditor}
               setOpenedEditor={setOpenedEditor}
               setHideTopButton={setHideTopButton}
@@ -169,15 +175,15 @@ const EmailsPopup = ({ handleClose, threadData, setInboxData, contactEmail, inbo
           </div>
           <div className={'h-[5px] border-y border-gray2 relative'}>
             <div className={'absolute ml-3 top-[-16px] cursor-pointer'} onClick={() => setShowAll(true)}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
-                <circle cx="16" cy="16" r="15.5" fill="#F9FAFB" stroke="#D1D5DB" />
+              <svg xmlns='http://www.w3.org/2000/svg' width='32' height='32' viewBox='0 0 32 32' fill='none'>
+                <circle cx='16' cy='16' r='15.5' fill='#F9FAFB' stroke='#D1D5DB' />
                 <path
-                  d="M16.1351 21.3298L19.8649 17.6001L21 18.7352L16.1351 23.6001L11.2703 18.7352L12.4054 17.6001L16.1351 21.3298Z"
-                  fill="#4B5563"
+                  d='M16.1351 21.3298L19.8649 17.6001L21 18.7352L16.1351 23.6001L11.2703 18.7352L12.4054 17.6001L16.1351 21.3298Z'
+                  fill='#4B5563'
                 />
                 <path
-                  d="M15.8649 11.2703L12.1351 15L11 13.8649L15.8649 9L20.7297 13.8649L19.5946 15L15.8649 11.2703Z"
-                  fill="#4B5563"
+                  d='M15.8649 11.2703L12.1351 15L11 13.8649L15.8649 9L20.7297 13.8649L19.5946 15L15.8649 11.2703Z'
+                  fill='#4B5563'
                 />
               </svg>
             </div>
@@ -188,13 +194,13 @@ const EmailsPopup = ({ handleClose, threadData, setInboxData, contactEmail, inbo
                 <EmailItem
                   inboxData={inboxData}
                   subject={threadData[threadData?.length - 1]?.subject}
-                  fromEmail={threadData[threadData?.length - 1]?.from_email}
+                  fromEmail={threadData[index]?.from_email}
                   contactEmail={contactEmail}
                   setInboxData={setInboxData}
                   threadId={e?.thread_id}
                   message_header_id={e?.message_header_id}
                   sentDate={timeAgo(e?.sent_date)}
-                  name={`${threadData[index]?.from_first_name} ${threadData[threadData[index]]?.from_last_name}`}
+                  name={`${threadData[index]?.from_first_name} ${threadData[index]?.from_last_name}`}
                   isLast={index === threadData?.slice(-2).length - 1}
                   body={e?.html_body?.length > 0 ? e?.html_body : e?.body}
                   email={threadData[index]?.from_email}
@@ -224,7 +230,7 @@ const EmailsPopup = ({ handleClose, threadData, setInboxData, contactEmail, inbo
                     threadId={e?.thread_id}
                     message_header_id={e?.message_header_id}
                     sentDate={timeAgo(e?.sent_date)}
-                    name={`${threadData[index]?.from_first_name}  ${threadData[threadData[index]]?.from_last_name}`}
+                    name={`${threadData[index]?.from_first_name}  ${threadData[index]?.from_last_name}`}
                     isLast={index === threadData?.length - 1}
                     body={e?.html_body?.length > 0 ? e?.html_body : e?.body}
                     email={threadData[index]?.from_email}
