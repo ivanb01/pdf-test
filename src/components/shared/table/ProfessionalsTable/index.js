d4582279b8b0fa7448b0b04dece093c70ddc8e5f
blendar-opgny <82260563+blendar-opgny@users.noreply.github.com>
2024-05-16 10:58:32 +0200

    fix professionals table (#452)

--- src/components/shared/table/ProfessionalsTable/index.js
+++ src/components/shared/table/ProfessionalsTable/index.js
@@ -70,7 +70,7 @@ const ProfessionalsTable = ({ data, tableFor, categoryType, handleCardEdit, sear
         <tr>
           <th
             scope="col"
-            className="py-3 pl-4 pr-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500 sm:pl-6 flex items-center">
+            className="py-3 pl-4 pr-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500 sm:pl-6 flex items-center min-w-[500px]">
             {/* <Input
               type="checkbox"
               onChange={(event) => handleSelectContact(event, contact)}
