71ad0f752d38e29212df72540b3a7e0bbdccccf9
blendar-opgny <82260563+blendar-opgny@users.noreply.github.com>
2024-05-23 11:03:03 +0200

    fix edit icon (#494)

--- src/components/shared/table/ProfessionalsTable/index.js
+++ src/components/shared/table/ProfessionalsTable/index.js
@@ -46,10 +46,10 @@ const ProfessionalsTable = ({ data, tableFor, categoryType, handleCardEdit, sear
     openedSubtab === 0
       ? vendorSubtypes
       : openedSubtab === 1
-        ? agentTypes
-        : openedSubtab === 3
-          ? unspecifiedTypes
-          : vendorSubtypes && [...vendorSubtypes, ...agentTypes, ...unspecifiedTypes];
+      ? agentTypes
+      : openedSubtab === 3
+      ? unspecifiedTypes
+      : vendorSubtypes && [...vendorSubtypes, ...agentTypes, ...unspecifiedTypes];
   const [openCommuncationPopup, setOpenCommunicationPopup] = useState(false);
   const hideUnapproved = useSelector((state) => state.global.hideUnapproved);
 
@@ -70,8 +70,7 @@ const ProfessionalsTable = ({ data, tableFor, categoryType, handleCardEdit, sear
         <tr>
           <th
             scope="col"
-            className="py-3 pl-4 pr-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500 sm:pl-6 flex items-center min-w-[500px]"
-          >
+            className="py-3 pl-4 pr-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500 sm:pl-6 flex items-center min-w-[500px]">
             {/* <Input
               type="checkbox"
               onChange={(event) => handleSelectContact(event, contact)}
@@ -86,8 +85,7 @@ const ProfessionalsTable = ({ data, tableFor, categoryType, handleCardEdit, sear
           </th> */}
           <th
             scope="col"
-            className="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500 min-w-[400px]"
-          >
+            className="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500 min-w-[400px]">
             Contact summary
           </th>
           <th scope="col" className="px-3 py-3 text-center text-xs font-medium uppercase tracking-wide text-gray-500">
@@ -132,8 +130,7 @@ const ProfessionalsTable = ({ data, tableFor, categoryType, handleCardEdit, sear
                           pathname: '/contacts/details',
                           query: { id: contact.id },
                         })
-                      }
-                    >
+                      }>
                       <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
                         <ContactInfo
                           data={{
@@ -163,12 +160,10 @@ const ProfessionalsTable = ({ data, tableFor, categoryType, handleCardEdit, sear
                               <div
                                 className={
                                   'max-w-[239px] leading-5 text-left font-medium text-[11px] px-3 py-0.5 mt-1.5 text-ellipsis overflow-hidden bg-lightBlue1 text-lightBlue3 '
-                                }
-                              >
+                                }>
                                 {contact.summary}
                               </div>
-                            }
-                          >
+                            }>
                             <div className={`w-[260px] pointer-events-none text-white bg-neutral1 rounded-lg`}>
                               <p className="text-xs leading-4 font-normal">{contact.summary}</p>
                             </div>
@@ -177,35 +172,26 @@ const ProfessionalsTable = ({ data, tableFor, categoryType, handleCardEdit, sear
                       </td>
                       <td>
                         <div className="px-4 py-[10px] flex items-center justify-center">
-                          <div
-                            className="group cursor-pointer relative rounded-full p-1.5 bg-lightBlue1 hover:bg-lightBlue2 mr-2 flex items-center justify-center"
-                            onMouseEnter={() => {
-                              document
-                                .querySelector('#tooltip-edit-contact-' + contact.id)
-                                .classList.remove('invisible', 'opacity-0');
-                              document.querySelector('#edit-contact-icon-' + contact.id).classList.add('text-gray4');
-                              document.querySelector('#edit-contact-icon-' + contact.id).classList.remove('text-gray3');
-                            }}
-                            onMouseLeave={() => {
-                              document
-                                .querySelector('#tooltip-edit-contact-' + contact.id)
-                                .classList.add('invisible', 'opacity-0');
-                              document.querySelector('#edit-contact-icon-' + contact.id).classList.add('text-gray3');
-                              document.querySelector('#edit-contact-icon-' + contact.id).classList.remove('text-gray4');
-                            }}
-                            onClick={(e) => {
-                              e.stopPropagation();
-                              handleCardEdit(contact);
-                            }}
-                          >
-                            <Edit id={'edit-contact-icon-' + contact.id} className="text-lightBlue5 w-4 h-4" />
-                            <div
-                              id={'tooltip-edit-contact-' + contact.id}
-                              className="inline-block absolute bottom-[34px]  whitespace-nowrap invisible opacity-0 z-10 py-2 px-3 text-xs font-medium text-white bg-neutral1 rounded-lg shadow-sm dark:bg-gray-700 "
-                            >
-                              Edit Contact
-                            </div>
-                          </div>
+                          <TooltipComponent
+                            side={'top'}
+                            align="center"
+                            style={{ marginBottom: '7px' }}
+                            triggerElement={
+                              <div
+                                role={'button'}
+                                onClick={(e) => {
+                                  e.stopPropagation();
+                                  handleCardEdit(contact);
+                                }}
+                                className="group/edit cursor-pointer rounded-full p-1.5 bg-gray1 hover:bg-lightBlue2  mr-2 flex items-center justify-center">
+                                <Edit
+                                  id={'edit-contact-icon-' + contact.id}
+                                  className="group-hover/edit:text-lightBlue5 text-gray3 w-4 h-4"
+                                />
+                              </div>
+                            }>
+                            <div className={'text-xs leading-4 font-medium'}>Edit Contact</div>
+                          </TooltipComponent>
                         </div>
                       </td>
                     </tr>
