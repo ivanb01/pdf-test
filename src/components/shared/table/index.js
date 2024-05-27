8ec42ee108fc2bd88040a6cc7c54bd7f8b91f29c
erzaopgny <142299390+erzaopgny@users.noreply.github.com>
2024-05-27 13:25:07 +0200

    Feature/id 1029/campaign table (#514)
    * remove sms icon from preview of template
    
    * fix overflow issues in campaigns table

--- src/components/shared/table/index.js
+++ src/components/shared/table/index.js
@@ -3,7 +3,7 @@ const Table = ({ children, tableFor, isInCampaign }) => {
     <div className="h-full ">
       <div className="h-full flex flex-col">
         <div
-          className={`h-full ${isInCampaign && 'border border-gray2 rounded-md'} ${tableFor === 'categorized' ? 'overflow-x-hidden' : 'overflow-x-auto'}`}>
+          className={`h-full ${isInCampaign && 'border border-gray2 rounded-md'} ${tableFor === 'categorized' || tableFor === 'inCampaignContacts' ? 'overflow-x-hidden' : 'overflow-x-auto'}`}>
           <div className="h-full inline-block min-w-full align-middle">
             <div className="ring-black ring-opacity-5">
               <table className="min-w-full divide-y divide-gray-200">{children}</table>
