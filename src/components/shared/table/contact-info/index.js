18cd68dccf07bc44546a872758897281074db8fb
erzaopgny <142299390+erzaopgny@users.noreply.github.com>
2024-06-07 13:54:49 +0200

    Erza/id 1488 all contacts fix (#613)
    * make fields optional
    
    * make fields optional

--- src/components/shared/table/contact-info/index.js
+++ src/components/shared/table/contact-info/index.js
@@ -48,17 +48,16 @@ const ContactInfo = ({
           // <Image className="h-10 w-10 rounded-full" src={src} alt="" />
           <span className="inline-flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
             <span className="text-sm font-medium leading-none sss text-white">
-              {getInitials(data.name).toUpperCase()}
+              {getInitials(data?.name).toUpperCase()}
             </span>
           </span>
         )}
       </div>
       <div className="ml-3">
         <div
-          className={`font-medium text-gray7 flex  ${maxWidth && `lg-w-[${maxWidth}]`}  xl:min-w-[100px] lg:w-[150px]`}
-        >
+          className={`font-medium text-gray7 flex  ${maxWidth && `lg-w-[${maxWidth}]`}  xl:min-w-[100px] lg:w-[150px]`}>
           <p className={'word-break overflow-hidden '} style={{ textOverflow: 'ellipsis' }}>
-            {data.name}
+            {data?.name}
           </p>{' '}
           {showAIChip && <AIChip className="ml-2" reviewed={data.approved_ai} />}
         </div>
@@ -67,8 +66,7 @@ const ContactInfo = ({
             title={data.email}
             className={`ellipsis-email text-gray-500 font-medium ${
               maxWidth && `xl:min-w-[${maxWidth}]`
-            } xl:min-w-[300px] lg:w-[120px] flex `}
-          >
+            } xl:min-w-[300px] lg:w-[120px] flex `}>
             {emailHover && (
               <TooltipComponent
                 side={'bottom'}
@@ -77,8 +75,7 @@ const ContactInfo = ({
                   <div className={`ellipsis-email ${maxWidth && `lg:w-[${maxWidth}]`} xl:min-w-[150px] lg:w-[130px]`}>
                     {data.email}
                   </div>
-                }
-              >
+                }>
                 <div className={'text-sm'}>{data.email}</div>
               </TooltipComponent>
             )}
@@ -97,8 +94,7 @@ const ContactInfo = ({
                         style={{ height: '15px', width: '15px' }}
                         aria-hidden="true"
                       />
-                    }
-                  >
+                    }>
                     <div className={`max-w-[260px] pointer-events-none text-white bg-neutral1 rounded-lg`}>
                       <div className={`flex gap-1.5`}>
                         {getSource(data.import_source_text).icon}
@@ -120,8 +116,7 @@ const ContactInfo = ({
                   <div className={`ellipsis-email xl:min-w-[300px] lg:w-[130px] ${maxWidth && `lg:w-[${maxWidth}]`} `}>
                     {data.email}
                   </div>
-                }
-              >
+                }>
                 <div className={'text-sm'}>{data.email}</div>
               </TooltipComponent>
             ) : (
