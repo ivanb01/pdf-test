7eb6dbf66b573586824aa6f19c49b8e0f01bcc97
erzaopgny <142299390+erzaopgny@users.noreply.github.com>
2024-06-13 11:32:41 +0200

    [Dev Release] - F/erza/admin dashboard (#658)
    * remove sms icon from preview of template
    
    * add admin dashboard
    
    * add admin dashboard
    
    * implement dashboard page
    
    * implement dashboard page
    
    * implement dashboard page
    
    * fix redirect issues
    
    * add agentDashboard
    
    * fix agent dashboard feedback
    
    * fix agent dashboard feedback
    
    * fix application error
    
    * fix ui
    
    * fix agent dashboard layout
    
    * redesign Agent Dashboard page
    
    * fix layout

--- src/components/Layout/AgentDashboardLayout/index.js
+++ src/components/Layout/AgentDashboardLayout/index.js
@@ -32,56 +32,67 @@ const AgentDashboardLayout = ({ success, needToContactCount, needToReview }) => 
           }) && d.category_1 === category,
       );
     };
+    const noResults = uniqueCategories.every((category) => {
+      const filteredItems = searchItems(category);
+      return filteredItems.length === 0;
+    });
 
+    if (searchTerm.length > 0 && noResults) {
+      return (
+        <div className="min-w-[300px] md:min-w-[600px]  z-[9999]  p-3 bg-white text-center h-[200px] flex flex-col items-center justify-center rounded-lg">
+          <h6 className="text-sm leading-5 font-medium text-gray-900 mb-[11px]">No results found</h6>
+          <p className="text-sm leading-5 font-normal text-gray-600">
+            We can’t find anything with that term at the moment,
+            <br />
+            try searching something else.
+          </p>
+        </div>
+      );
+    }
     return uniqueCategories.map((category) => {
       const filteredItems = searchItems(category);
       if (filteredItems.length > 0) {
         return (
-          <div key={category} className={'absolute z-99999 w-[600px]'}>
-            <SimpleBar
-              className="rounded-lg bg-white overflow-y-auto overflow-x-hidden  border border-gray2"
-              style={{ marginTop: '5px', maxHeight: '276px' }}>
-              <h2 className="text-xs leading-5 font-medium py-2.5 px-[13px] bg-gray-50">{category.toUpperCase()}</h2>
-              <ul className="bg-white z-10">
-                {filteredItems.map((item) => (
-                  <li
-                    key={item.id}
-                    role="button"
-                    onClick={() => {
-                      onClose();
-                      router.push({
-                        pathname: '/contacts/details',
-                        query: { id: item?.id },
-                      });
-                    }}
-                    className="py-2 px-3 flex gap-3 justify-between items-center hover:bg-lightBlue1 cursor-pointer">
-                    {item.profile_image_path ? (
-                      <img
-                        className="inline-block h-7 w-7 rounded-full"
-                        src={item.profile_image_path}
-                        alt={item.first_name}
-                      />
-                    ) : (
-                      <span className="inline-flex h-6 w-6  items-center justify-center rounded-full bg-gray-400">
-                        <span className="text-sm font-medium leading-none text-white">
-                          {getInitials(item.first_name + ' ' + item.last_name).toUpperCase()}
-                        </span>
+          <>
+            <h2 className="text-xs leading-5 font-medium py-2.5 px-[13px] bg-gray-50">{category.toUpperCase()}</h2>
+            <ul className="bg-white z-10">
+              {filteredItems.map((item) => (
+                <li
+                  key={item.id}
+                  role="button"
+                  onClick={() => {
+                    router.push({
+                      pathname: '/contacts/details',
+                      query: { id: item?.id },
+                    });
+                  }}
+                  className="py-2 px-3 flex gap-3 justify-between items-center hover:bg-lightBlue1 cursor-pointer">
+                  {item.profile_image_path ? (
+                    <img
+                      className="inline-block h-7 w-7 rounded-full"
+                      src={item.profile_image_path}
+                      alt={item.first_name}
+                    />
+                  ) : (
+                    <span className="inline-flex h-6 w-6  items-center justify-center rounded-full bg-gray-400">
+                      <span className="text-sm font-medium leading-none text-white">
+                        {getInitials(item.first_name + ' ' + item.last_name).toUpperCase()}
                       </span>
-                    )}
-                    <div className="flex-1">
-                      <h5 className="text-sm leading-5 font-normal">
-                        {item.first_name} {item.last_name}
-                      </h5>
-                      <h6 className="text-xs leading-4 font-normal text-gray-500">{item.email}</h6>
-                    </div>
-                    <div className="px-1.5 py-1 rounded bg-gray-100">
-                      <h1 className="font-medium text-gray-800 uppercase leading-4 text-[10px]">{item.category_2}</h1>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </SimpleBar>
-          </div>
+                    </span>
+                  )}
+                  <div className="flex-1">
+                    <h5 className="text-sm leading-5 font-normal">
+                      {item.first_name} {item.last_name}
+                    </h5>
+                    <h6 className="text-xs leading-4 font-normal text-gray-500">{item.email}</h6>
+                  </div>
+                  <div className="px-1.5 py-1 rounded bg-gray-100">
+                    <h1 className="font-medium text-gray-800 uppercase leading-4 text-[10px]">{item.category_2}</h1>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </>
         );
       }
 
@@ -109,7 +120,7 @@ const AgentDashboardLayout = ({ success, needToContactCount, needToReview }) => 
 
     if (searchTerm.length > 0 && noResults) {
       return (
-        <div className=" w-[600px] border border-gray2 mt-[5px] absolute p-3 bg-white text-center h-[200px] flex flex-col items-center justify-center rounded-lg">
+        <div className="min-w-[300px] md:min-w-[600px] border border-gray2 mt-[5px] z-[9999] absolute p-3 bg-white text-center h-[200px] flex flex-col items-center justify-center rounded-lg">
           <h6 className="text-sm leading-5 font-medium text-gray-900 mb-[11px]">No results found</h6>
           <p className="text-sm leading-5 font-normal text-gray-600">
             We can’t find anything with that term at the moment,
@@ -128,7 +139,7 @@ const AgentDashboardLayout = ({ success, needToContactCount, needToReview }) => 
         style={{ height: '270px' }}>
         <div className={'flex max-w-[570px] flex-col justify-center items-center gap-5'}>
           <h3 className={'text-4xl leading-9 font-semibold text-white'}>Welcome {userInfo?.first_name ?? ''}!</h3>
-          <p className={'text-[12px] text-center font-medium mt-4 text-white'}>
+          <p className={'text-[12px] text-center font-medium mt-4 text-white px-3'}>
             We’re here to help you close more deals, stay on top of your client relationships & make sure you love where
             you work! Any feedback on how we can improve is encouraged and welcome!
           </p>
@@ -137,11 +148,17 @@ const AgentDashboardLayout = ({ success, needToContactCount, needToReview }) => 
           <Search
             disabled={!contactsData}
             placeholder="Search for a contact..."
-            className="text-sm w-[600px]"
+            className="text-sm min-w-[300px] md:min-w-[600px]"
             onChange={(event) => setSearchTerm(event.target.value)}
           />
-          {searchTerm.length > 0 && <>{renderSearchResults()}</>}
-          {searchTerm.length > 0 && renderNoResultsMessage()}
+          {searchTerm.length > 0 && (
+            <div
+              className="absolute rounded-lg bg-white overflow-y-auto overflow-x-hidden min-w-[300px] md:min-w-[600px] z-50 border border-gray2"
+              style={{ marginTop: '5px', maxHeight: '276px' }}>
+              {renderSearchResults()}
+            </div>
+          )}
+          {/*{searchTerm.length > 0 && renderNoResultsMessage()}*/}
         </div>
       </div>
       <div className={'flex gap-3 mx-9 mt-5'}>
