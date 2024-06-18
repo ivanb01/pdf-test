928422c35c55ab9068bba5a52dac881178078a57
Edon Mustafa <edon@opgny.com>
2024-06-18 11:30:14 +0200

    Revert "maintenance prod on"
    This reverts commit 03b124a458f345798dd761652fc0de109d387a35.

--- src/components/withAuth.js
+++ src/components/withAuth.js
@@ -11,7 +11,7 @@ export default function withAuth(Component) {
       if (typeof window !== 'undefined') {
         const isAuthenticated = localStorage.getItem('isAuthenticated');
 
-        let maintenance = true;
+        let maintenance = false;
         if (maintenance === true) {
           router.push('/maintenance');
         }
